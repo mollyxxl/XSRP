d5781342023d6a852bf38c1ebd2b44c120fc300c
mollyxxl <1187820970@qq.com>
2020-08-05 19:03:59 +0800

    X Pipeline

---
+++ Assets/Scripts/XPipeline.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
+public class XPipeline : RenderPipeline
+{
+    CullResults cull;
+    CommandBuffer cameraBuffer = new CommandBuffer()
+    {
+        name = "Render Camera"
+    };
+
+    Material errorMaterial;
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+
+        foreach (var camera in cameras)
+        {
+            Render(renderContext,camera);
+        }
+    }
+    void Render(ScriptableRenderContext context, Camera camera)
+    {
+        ScriptableCullingParameters cullingParameters;
+        if (!CullResults.GetCullingParameters(camera, out cullingParameters))
+        {
+            return;
+        }
+#if UNITY_EDITOR
+        //SceneView 处理UI
+        if(camera.cameraType== CameraType.SceneView)
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+#endif
+        CullResults.Cull(ref cullingParameters, context,ref cull);
+
+        context.SetupCameraProperties(camera);  //设置视图投影矩阵
+
+        CameraClearFlags clearFlags = camera.clearFlags;
+        cameraBuffer.ClearRenderTarget(
+            (clearFlags&CameraClearFlags.Depth) !=0, 
+            (clearFlags&CameraClearFlags.Color) !=0,
+            camera.backgroundColor);
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear(); 
+
+        var drawSetting = new DrawRendererSettings(
+            camera,new ShaderPassName("SRPDefaultUnlit")
+            ) ;
+        drawSetting.sorting.flags = SortFlags.CommonOpaque;
+        var filterSettings = new FilterRenderersSettings(true)
+        {
+            renderQueueRange = RenderQueueRange.opaque
+        };
+        context.DrawRenderers(cull.visibleRenderers, ref drawSetting, filterSettings);
+        context.DrawSkybox(camera);
+
+        drawSetting.sorting.flags = SortFlags.CommonTransparent;
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cull.visibleRenderers, ref drawSetting, filterSettings);
+
+        DrawDefaultPipeline(context, camera);
+        
+        context.Submit();
+    }
+    [Conditional("DEVELOPMENT_BUILD"),Conditional("UNITY_EDITOR")]
+     void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera)
+    {
+        if (errorMaterial == null)
+        {
+            Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        var drawSettings = new DrawRendererSettings(camera, new ShaderPassName("ForwardBase"));
+        drawSettings.SetShaderPassName(1, new ShaderPassName("PrepassBase"));
+        drawSettings.SetShaderPassName(2, new ShaderPassName("Always"));
+        drawSettings.SetShaderPassName(3, new ShaderPassName("Vertex"));
+        drawSettings.SetShaderPassName(4, new ShaderPassName("VertexLMRGB"));
+        drawSettings.SetShaderPassName(5, new ShaderPassName("VertexLM"));
+        drawSettings.SetOverrideMaterial(errorMaterial, 0);
+
+        var filterSettings = new FilterRenderersSettings(true);
+        context.DrawRenderers(cull.visibleRenderers, ref drawSettings, filterSettings);
+    }
+}
---
+++ Assets/Scripts/XPipelineAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[CreateAssetMenu(menuName ="Rendering/X Pipeline")]
+public class XPipelineAsset : RenderPipelineAsset
+{
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new XPipeline();
+    }
+}
